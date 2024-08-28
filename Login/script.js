b52ec8988db7aa6b3a67b60ef9fd1d0c00f424da
ingridcriistine <ingridrochacristine@gmail.com>
2024-08-28 16:51:28 -0300

    login feature 01

--- Login/script.js
+++ Login/script.js
@@ -1,3 +1,25 @@
+modalValidacao = document.getElementById("modal-validacao");
+modalValidacao.style.display = "none";
+
+input1 = document.getElementById("nome");
+input2 = document.getElementById("senha");
+
+const modal = document.createElement("div");
+modal.className = "box-modal";
+modal.style.width = "18rem";
+
+const boxTitle = document.createElement("h5");
+boxTitle.className = "box-title";
+
+const boxButton = document.createElement("a");
+boxButton.className = "box-a";
+boxButton.textContent = "Fechar";
+
+modal.appendChild(boxTitle);
+modal.appendChild(boxButton);
+
+modalValidacao.appendChild(modal);
+
 function login() {
   var nome = $("#nome").val();
   var senha = $("#senha").val();
@@ -12,7 +34,15 @@ function login() {
     localStorage.setItem("usuario", JSON.stringify(user));
 
     window.location.href = "../Loja/loja.html";
+
   } else {
     //logica para se o nome e senha forem incorretos
+    modalValidacao.style.display = "block";
+    modalValidacao.style.background = "rgb(233, 77, 77)"
+    boxTitle.textContent = "Credenciais incorretas!";
+    input1.style.border = "2px solid red";
+    input2.style.border = "2px solid red";
   }
 }
+
+
