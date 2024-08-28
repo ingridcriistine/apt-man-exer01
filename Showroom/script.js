bb989b701ebe2b6492bed7f0f826c829da5256d4
ingridcriistine <ingridrochacristine@gmail.com>
2024-08-28 15:50:06 -0300

    showroom feature 01

---
+++ Showroom/script.js
@@ -0,0 +1,54 @@
+// Declaração da variável produtos fora do escopo do evento para torná-la global
+let produtos;
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("../Dados/showroom.json")
+    .then((response) => response.json())
+    .then((data) => {
+      produtos = data;
+      const produtosContainer =
+        document.getElementById("produtos-container");
+
+      produtos.map((produto, index) => {
+        const card = document.createElement("div");
+        card.className = "card-produto";
+        card.style.width = "18rem";
+        card.style.marginRight = "10px";
+
+        const imagem = document.createElement("img");
+        imagem.src = produto.imagem;
+        imagem.className = "card-img";
+
+        const cardBody = document.createElement("div");
+        cardBody.className = "card-body";
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.className = "card-title";
+        cardTitle.textContent = produto.descricao;
+
+        const cardPreco = document.createElement("p");
+        cardPreco.className = "card-preco";
+        cardPreco.textContent = "Preço: $" + produto.preco.toFixed(2);
+
+        const cardQtd = document.createElement("p");
+        cardQtd.className = "card-qtd";
+        cardQtd.textContent = "Quantidade: " + produto.quantidade;
+
+        const cardStatus = document.createElement("p");
+        cardStatus.className = "card-status";
+        cardStatus.textContent = "Status: " + produto.status;
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardPreco);
+        cardBody.appendChild(cardQtd);
+        cardBody.appendChild(cardStatus);
+
+        card.appendChild(imagem);
+        card.appendChild(cardBody);
+
+        produtosContainer.appendChild(card);
+      });
+    })
+
+    .catch((error) => console.error("Erro ao carregar o arquivo JSON", error));
+});
